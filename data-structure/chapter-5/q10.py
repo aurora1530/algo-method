3315d5d9ae0d1d72179bf7094253541cc8bba900
aurora1530 <k.0523.y.1530@gmail.com>
2024-01-01 19:14:13 +0900

    chapter5 q10

--- data-structure/chapter-5/q10.py
+++ data-structure/chapter-5/q10.py
@@ -2,21 +2,18 @@ N = int(input())
 A = list(map(int, input().split()))
 
 MAX_NUM = 5 * (10 ** 5)
-counter = [-1 for _ in range(MAX_NUM)]
-
-# for a in A:
-#     for d in range(1, a + 1):
-#         if a % d == 0:
-#             counter[d] += 1
+counter = [0 for _ in range(MAX_NUM+1)]
+temp = [-1 for _ in range(MAX_NUM+1)]
+for a in A:
+    counter[a] += 1
 
 Q = int(input())
 
 for _ in range(Q):
     x = int(input())
-    if counter[x] == -1:
-        counter[x] = 0
-        for a in A:
-            if a % x == 0:
-                counter[x] += 1
-
-    print(counter[x])
+    if temp[x] == -1:
+        count = 0
+        for i in range(1, MAX_NUM//x + 1):
+            count += counter[i*x]
+        temp[x] = count
+    print(temp[x])
