260122fb73f45dbfab3f5c14918a247b5149722e
aurora1530 <k.0523.y.1530@gmail.com>
2024-01-01 12:38:42 +0900

    Refactor code for Runner class and update variable names

--- data-structure/chapter-4/4-7.py
+++ data-structure/chapter-4/4-7.py
@@ -1,16 +1,17 @@
 class Runner:
-    def __init__(self,num):
+    def __init__(self, num):
         self.fore = None
         self.back = None
-        self.num =num
+        self.num = num
         self.is_drop = False
+
     def drop(self):
         self.is_drop = True
 
 
-def overtake(runner:Runner):
+def overtake(runner: Runner):
     fore_runner = runner.fore
-    if fore_runner is None or runner.is_drop == True:
+    if fore_runner is None or runner.is_drop:
         return 'Error'
     fore_runner_num = fore_runner.num
 
@@ -21,14 +22,15 @@ def overtake(runner:Runner):
         fore_runner.fore.back = runner
 
     # 入れ替え
-    runner.fore = fore_runner.fore # fore_runner.foreがNoneのときも含む
+    runner.fore = fore_runner.fore  # fore_runner.foreがNoneのときも含む
     runner.back = fore_runner
     fore_runner.fore = runner
-    fore_runner.back = back_runner # back_runnerがNoneのときも含む
+    fore_runner.back = back_runner  # back_runnerがNoneのときも含む
 
     return fore_runner_num
 
-def dropout(runner:Runner):
+
+def dropout(runner: Runner):
     runner.drop()
     fore_runner = runner.fore
     back_runner = runner.back
@@ -40,20 +42,19 @@ def dropout(runner:Runner):
         fore_runner.back = back_runner
     return
 
-N=int(input())
-A = list(map(int,input().split()))
+
+N = int(input())
+A = list(map(int, input().split()))
 Q = int(input())
 
 runners = [Runner(i) for i in range(N)]
 for i in range(N):
     runner_num = A[i]
-    runners[runner_num].fore = None if i==0 else runners[A[i-1]]
-    runners[runner_num].back = None if i==N-1 else runners[A[i+1]]
-
-
+    runners[runner_num].fore = None if i == 0 else runners[A[i-1]]
+    runners[runner_num].back = None if i == N-1 else runners[A[i+1]]
 
 for i in range(Q):
-    query,v = map(int,input().split())
+    query, v = map(int, input().split())
     if query == 0:
         print(overtake(runners[v]))
     else:
