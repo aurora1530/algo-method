48811cc4b311ca205c95af9bb1af68d86cc37346
aurora1530 <k.0523.y.1530@gmail.com>
2024-01-03 14:08:19 +0900

    chapter8 q2

---
+++ data-structure/chapter-8/q2.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+parent = [0 for _ in range(N)]  # parent[i] iの親の頂点番号
+children = [[] for _ in range(N)]  # chidren[i] 頂点iを親に持つ頂点番号のリスト
+
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    children[a].append(b)
+    parent[b] = a
+
+for row in children:
+    row.sort()
+
+Q = int(input())
+for _ in range(Q):
+    v = int(input())
+    print(' '.join(list(map(int, children[parent[v]]))))
